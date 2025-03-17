977694358b396a192e4b83ffc709476c08e9ecbe
Atman Dhol <atman.dhol@broadcom.com>
2025-03-17 14:51:30 -0400

    chore: Bump versions

--- tappr/main.py
+++ tappr/main.py
@@ -21,7 +21,7 @@ from tappr.modules.utils.k8s import K8s
 from typing import Optional, List
 
 state = {"verbose": False, "context": None}
-VERSION = "0.21.0"
+VERSION = "0.22.0"
 
 typer_logger = TyperLogger()
 commons = Commons()
